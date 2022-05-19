50f064657a8327a8a427ef67016de903b3234918
Ilya Zelkin <ilya.zelkin.98@bk.ru>
2022-05-19 18:28:58 +0300

    AR

---
+++ LasVegas/LasVegas/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  LasVegas
+//
+//  Created by Ilya Zelkin on 19.05.2022.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        boxAnchor.generateCollisionShapes(recursive: true)
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
